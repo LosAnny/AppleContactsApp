7908e3f197e4fabb00ab4fb74e16f79d924f8a92
los.anny <ann.losh99@mail.ru>
2022-10-04 11:02:09 +0300

    The project was prepared for development at the MVC.

---
+++ AppleContactsApp/Sources/App/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  AppleContactsApp
+//
+//  Created by Анна Лошакова on 04.10.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+}
+
---
+++ AppleContactsApp/Sources/App/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  AppleContactsApp
+//
+//  Created by Анна Лошакова on 04.10.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+}
+
